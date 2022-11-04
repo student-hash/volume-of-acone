21d46cb9441faeb434d0752f6cec66f748633945
student-hash <98327528+student-hash@users.noreply.github.com>
2022-11-04 13:47:36 +0300

    Add files via upload

---
+++ my project klvn 2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main ()
+{ 
+ float b1,b2,h,a;
+ cout<<"enter base 1 of trapezoid :"<<endl;
+ cin>>b1;
+ cout<<"\n enter base 2 of trapezoid :"<<endl;
+ cin>>b2;
+ cout<<"\n enter height of trapezoid :"<<endl;
+ cin>>h;
+ a=1/2*(b1+b2)*h;
+ 
+ cout<<"enter area of trapezium is a:"<<endl;
+ return 0;
+}
